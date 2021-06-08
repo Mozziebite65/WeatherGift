de0903ed75db8742403fbada376039ee2321ec22
Rick Martin <richardmartin188@gmail.com>
2021-06-08 18:36:36 +0100

    Added WeatherLocation.swift

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Rick Martin on 08/06/2021.
+//
+
+import Foundation
+
+struct  WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longtitude: Double
+}
