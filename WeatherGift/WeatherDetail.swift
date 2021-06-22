c00a31112d2df16a1989b233eec1853effdcc19f
Rick Martin <richardmartin188@gmail.com>
2021-06-22 14:46:48 +0100

    Added daily JSON decoding

--- WeatherGift/WeatherDetail.swift
+++ WeatherGift/WeatherDetail.swift
@@ -7,33 +7,78 @@
 
 import Foundation
 
+// The dateFormatter() call is expensive, so we just want to make it once, outside the class
+// Doing this OUTSIDE the class definition creates an instance that persists between creation and destruction of instances of the class itself.
+// We can also set the dateFormat in here, as that will not change.
+private let dateFormatter: DateFormatter = {
+    
+    var dateFormatter = DateFormatter()
+    
+    // Lets format the output to full day, abbreviated month, numeric day and full year
+    // TO GET ANY DATE FORMATS WE WANT, GOOGLE "date format patterns unicode" and go to Unicode.org UTS #35 Dates - Date Format Patterns
+    
+    //dateFormatter.dateFormat = "EEE, MMM d, y H:mm"   // Short day, year, 24hr
+    dateFormatter.dateFormat = "EEEE"   // Full day
+
+    return dateFormatter
+    
+}()
+
+struct DailyWeather {
+    
+    var dailyIcon: String
+    var dailyWeekday: String
+    var dailySummary: String
+    var dailyHigh: Int
+    var dailyLow: Int
+    
+}
+
 class WeatherDetail: WeatherLocation {
     
     // Structs to hold the weather data from the OpenWeather API data
     // Each "nested" level of data in the JSON has it's own struct
     
     // The top level
-    struct Result: Codable {
+    private struct Result: Codable {
         
         var timezone: String
         var current: Current
+        var daily: [Daily]
         
     }
     
-    // First nested level1 "current"
-    struct Current: Codable {
+    // First nested level1 - "current"
+    private struct Current: Codable {
         
-        var dt: Double
+        var dt: TimeInterval
         var temp: Double
         var weather: [Weather]
         
     }
     
     // Second nested level
-    struct Weather: Codable {
+    private struct Weather: Codable {
         
         var description: String
         var icon: String
+        
+    }
+    
+    // First nested level - "Daily"
+    private struct Daily: Codable {
+        
+        var dt: TimeInterval
+        var temp: Temp
+        var weather: [Weather]
+        
+    }
+    
+    // Second nested level for "Daily"
+    private struct Temp: Codable {
+        
+        var min: Double
+        var max: Double
         
     }
     
@@ -41,14 +86,15 @@ class WeatherDetail: WeatherLocation {
     var currentTime = 0.0
     var temperature = 0
     var summary = ""
-    var dailyIcon = ""
+    var dayIcon = ""
+    var dailyWeatherData: [DailyWeather] = []
     
     func getData(completed: @escaping () -> ()) {
         
         // the API call, taken directly from the openweather site. Note the "exclude" parameter, as we don't want the "minutely" part of the JSON. We can also change the unit of temperature from Kelvin to Celcius.
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longtitude)&exclude=minutely&units=metric&appid=\(APIkeys.openWeatherAPIKey)"
         
-        //print("🕸 We are accessing the url \(urlString)")
+        print("🕸 We are accessing the url \(urlString)")
         
         // Create a URL
         guard let url = URL(string: urlString) else {
@@ -77,13 +123,37 @@ class WeatherDetail: WeatherLocation {
                 // Decode the JSON data
                 let decoder = JSONDecoder()
                 let result = try decoder.decode(Result.self, from: data!)       // "decode" throws an error, so we're inside a do-catch
-//                print("😎 \(result)")
-//                print("The timezone for \(self.name) is \(result.timezone)")
+                //print("😎 \(result)")
+                
+                // The properties for the "current" weather
                 self.timezone = result.timezone
                 self.currentTime = result.current.dt
                 self.temperature = Int(result.current.temp.rounded())
                 self.summary = result.current.weather[0].description
-                self.dailyIcon = self.fileNameForIcon(openWeatherIconName: result.current.weather[0].icon)
+                self.dayIcon = self.fileNameForIcon(openWeatherIconName: result.current.weather[0].icon)
+                
+                // The properties for the "daily" weather array
+                print("There are \(result.daily.count) days in the daily array...")
+
+                for index in 0..<result.daily.count {
+                    let weekDay = Date(timeIntervalSince1970: result.daily[index].dt)
+                    // Adjust for time zone
+                    dateFormatter.timeZone = TimeZone(identifier: result.timezone)
+                    
+                    let dailyweather = DailyWeather(dailyIcon: self.fileNameForIcon(openWeatherIconName: result.daily[index].weather[0].icon),
+                                                    dailyWeekday: dateFormatter.string(from: weekDay),
+                                                    dailySummary: result.daily[index].weather[0].description,
+                                                    dailyHigh: Int(result.daily[index].temp.max.rounded()),
+                                                    dailyLow: Int(result.daily[index].temp.min.rounded())
+                    )
+                 
+                    print(dailyweather)
+                    self.dailyWeatherData.append(dailyweather)
+                    
+                }
+                
+                
+                
                 
             } catch {
                 // if an error is thrown
