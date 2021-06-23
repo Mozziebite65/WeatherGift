2e270a3bc08b585c6cbe58cd938eea38e8154b94
Rick Martin <richardmartin188@gmail.com>
2021-06-23 17:03:22 +0100

    Added Hourly JSON parsing to WeatherDetail.swift

--- WeatherGift/WeatherDetail.swift
+++ WeatherGift/WeatherDetail.swift
@@ -22,7 +22,21 @@ private let dateFormatter: DateFormatter = {
 
     return dateFormatter
     
-}()
+} ()
+
+private let hourFormatter: DateFormatter = {
+    
+    var hourformatter = DateFormatter()
+    
+    // Lets format the output to full day, abbreviated month, numeric day and full year
+    // TO GET ANY DATE FORMATS WE WANT, GOOGLE "date format patterns unicode" and go to Unicode.org UTS #35 Dates - Date Format Patterns
+    
+    //dateFormatter.dateFormat = "EEE, MMM d, y H:mm"   // Short day, year, 24hr
+    hourformatter.dateFormat = "ha"   // hour, AM / PM
+
+    return hourformatter
+    
+} ()
 
 struct DailyWeather {
     
@@ -31,6 +45,14 @@ struct DailyWeather {
     var dailySummary: String
     var dailyHigh: Int
     var dailyLow: Int
+    
+}
+
+struct HourlyWeather {
+    
+    var hourlyIcon: String
+    var hour: String
+    var hourlyTemperature: Int
     
 }
 
@@ -45,7 +67,7 @@ class WeatherDetail: WeatherLocation {
         var timezone: String
         var current: Current
         var daily: [Daily]
-        
+        var hourly: [Hourly]
     }
     
     // First nested level1 - "current"
@@ -82,19 +104,31 @@ class WeatherDetail: WeatherLocation {
         
     }
     
+    // First nested level - "Hourly"
+    private struct Hourly: Codable {
+        
+        var dt: TimeInterval
+        var temp: Double
+        var weather: [Weather]
+        
+    }
+    
+    
     var timezone = ""
     var currentTime = 0.0
     var temperature = 0
     var summary = ""
     var dayIcon = ""
     var dailyWeatherData: [DailyWeather] = []
+    var hourlyWeatherData: [HourlyWeather] = []
+    
     
     func getData(completed: @escaping () -> ()) {
         
         // the API call, taken directly from the openweather site. Note the "exclude" parameter, as we don't want the "minutely" part of the JSON. We can also change the unit of temperature from Kelvin to Celcius.
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longtitude)&exclude=minutely&units=metric&appid=\(APIkeys.openWeatherAPIKey)"
         
-        //print("🕸 We are accessing the url \(urlString)")
+        print("🕸 We are accessing the url \(urlString)")
         
         // Create a URL
         guard let url = URL(string: urlString) else {
@@ -136,7 +170,9 @@ class WeatherDetail: WeatherLocation {
                 //print("There are \(result.daily.count) days in the daily array...")
 
                 for index in 0..<result.daily.count {
+                    
                     let weekDay = Date(timeIntervalSince1970: result.daily[index].dt)
+                    
                     // Adjust for time zone
                     dateFormatter.timeZone = TimeZone(identifier: result.timezone)
                     
@@ -152,6 +188,32 @@ class WeatherDetail: WeatherLocation {
                     
                 }
                 
+                // The properties for the "hourly" weather array
+                //print("There are \(result.hourly.count) hours in the hourly array...")
+                
+                // Get no more that 24 hours of hourly data
+                let lastHour = min(24, result.hourly.count)
+                
+                if lastHour > 0 {
+                    
+                    for index in 1...lastHour {
+                        
+                        //let weekDay = Date(timeIntervalSince1970: result.daily[index].dt)
+                        let hourlyDate = Date(timeIntervalSince1970: result.hourly[index].dt)
+                        
+                        // Adjust for time zone
+                        hourFormatter.timeZone = TimeZone(identifier: result.timezone)
+                     
+                        let hourlyWeather = HourlyWeather(hourlyIcon: self.fileNameForIcon(openWeatherIconName: result.hourly[index].weather[0].icon),
+                                                          hour: hourFormatter.string(from: hourlyDate),
+                                                          hourlyTemperature: Int(result.hourly[index].temp.rounded())
+                        )
+                        
+                        print("Hour: \(hourFormatter.string(from: hourlyDate)), Temperature: \(Int(result.hourly[index].temp.rounded())), Icon: \(self.fileNameForIcon(openWeatherIconName: result.hourly[index].weather[0].icon))")
+                        self.hourlyWeatherData.append(hourlyWeather)
+                    }
+                    
+                }
                 
                 
                 
